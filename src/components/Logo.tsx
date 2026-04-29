8ea656a9327d0e614c6d4f2bb34c1d828dd1a855
josephmyalla <chanangae@yandex.com>
2026-04-29 08:59:15 +0200

    updating favicon and logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import logocameraused from "../assets/logocameraused.jpeg"
 const Logo =()=>{
     return(
         
-        <span className="grid h-10 w-10 place-items-center rounded-md bg-red-700 text-gold-foreground font-display font-bold hover:w-40 hover:h-40">
+        <span className="grid h-10 w-10 place-items-center rounded-md bg-red-700 text-gold-foreground font-display font-bold hover:w-40 hover:h-40 hover:transition-all hover:duration-75">
           <img src={logocameraused} alt="Camera Used TZ"/>
           </span>
     )
